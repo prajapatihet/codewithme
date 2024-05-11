e32dde5edb987e231236611a66c0db86887570fe
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-11 22:28:41 +0530

    conversion

---
+++ conversion.js
@@ -0,0 +1,41 @@
+let score = '32';
+
+console.log(score,typeof score);
+
+let valueNumber = Number(score);
+
+console.log(valueNumber,typeof valueNumber);
+
+let scores = '32sv';
+
+let valueNumbers = Number(scores);
+
+console.log(valueNumbers,typeof valueNumbers);
+
+let score1 = null;
+
+console.log(score1,typeof score1);
+
+let valueNumber1 = Number(score1);
+
+console.log(valueNumber1,typeof valueNumber1);
+
+let scores1 = '32sv';
+
+let valueNumbers1 = Number(scores1);
+
+console.log(valueNumbers1,typeof valueNumbers1);
+
+let score2 = undefined;
+
+console.log(score2,typeof score2);
+
+let valueNumber2 = Number(score2);
+
+console.log(valueNumber2,typeof valueNumber2);
+
+let scores2 = '32sv';
+
+let valueNumbers2 = Number(scores2);
+
+console.log(valueNumbers2,typeof valueNumbers2);
