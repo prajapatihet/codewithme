a8a8e41f53aaafaa2c9d092e66ebf5794fad775c
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-11 19:28:02 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+let name = "Het"; // as String
+let age  = 15; // as int
+let isLoggedin = false; // as boolean
+
+/*
+    number => 2 ^ 53
+    bigint
+    string ""
+    boolean => true or false
+    null => standalone value
+    undefined => not assigned any value yet.
+    symbol => unique 
+
+    object 
+*/
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
