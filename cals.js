49848bfc3fc28e20213d336a05d68e133ccb7c4a
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-12 14:53:36 +0530

    math lib

---
+++ cals.js
@@ -0,0 +1,14 @@
+const balance = new Number(100.89);
+
+
+// console.log(balance.toFixed(1));
+
+min = 10;
+max =20;
+
+// Math.random();
+// It will gives random values between 0 and 1 inclusive 
+// If we want to generate same specific value within some specific range then we
+// use below mathematical operation.
+    
+console.log(Math.floor(Math.random() * (max-min + 1) + min))
